ff1f83ae42fb5297739f0fde8251adb9c30d6a6a
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2022-12-04 15:56:41 -0700

    day 2 - A friendly game of A B C X Y Z

--- day2/solution.zsh
+++ day2/solution.zsh
@@ -3,6 +3,9 @@
 OSCORE="0"
 PSCORE="0"
 
+###
+### Part one
+###
 IFS=$'\n'
 for i in $(cat ./OGinput | grep 'B X'); do
   OSCORE=$((OSCORE+6))
@@ -52,5 +55,47 @@ done
 for i in $(cat ./OGinput | grep 'Z'); do
   PSCORE=$((PSCORE+3))
 done
+echo "$OSCORE $PSCORE"
 
+OSCORE=0
+PSCORE=0
+##
+## Part two
+##
+for i in $(cat ./OGinput | grep 'A X'); do
+  OSCORE=$((OSCORE+7))
+  PSCORE=$((PSCORE+3))
+done
+for i in $(cat ./OGinput | grep 'B X'); do
+  OSCORE=$((OSCORE+8))
+  PSCORE=$((PSCORE+1))
+done
+for i in $(cat ./OGinput | grep 'C X'); do
+  OSCORE=$((OSCORE+9))
+  PSCORE=$((PSCORE+2))
+done
+for i in $(cat ./OGinput | grep 'A Z'); do
+  PSCORE=$((PSCORE+8))
+  OSCORE=$((OSCORE+1))
+done
+for i in $(cat ./OGinput | grep 'B Z'); do
+  PSCORE=$((PSCORE+9))
+  OSCORE=$((OSCORE+2))
+done
+for i in $(cat ./OGinput | grep 'C Z'); do
+  PSCORE=$((PSCORE+7))
+  OSCORE=$((OSCORE+3))
+done
+for i in $(cat ./OGinput | grep 'A Y'); do
+  PSCORE=$((PSCORE+4))
+  OSCORE=$((OSCORE+4))
+done
+for i in $(cat ./OGinput | grep 'B Y'); do
+  PSCORE=$((PSCORE+5))
+  OSCORE=$((OSCORE+5))
+done
+for i in $(cat ./OGinput | grep 'C Y'); do
+  PSCORE=$((PSCORE+6))
+  OSCORE=$((OSCORE+6))
+done
 echo "$OSCORE $PSCORE"
