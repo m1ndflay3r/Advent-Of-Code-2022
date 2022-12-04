13e892c37ff6ff0202a79f3080f903d2f2955579
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2022-12-04 10:36:53 -0700

    misc

--- day1/solution.zsh
+++ day1/solution.zsh
@@ -4,6 +4,7 @@ source /$(pwd)/libraw
 
 COUNT=0
 FOUND=0
+TBTCOUNT=0
 until [ $COUNT -gt 250 ]; do
   TBT=$(eval echo $"elf$COUNT")
   zCOUNT=0
@@ -18,9 +19,73 @@ until [ $COUNT -gt 250 ]; do
     zCOUNT=$((zCOUNT+1))
   done
   if [ $FOUND = "1" ]; then
-    echo $TBT
+    echo "Biggest hoarder is elf $COUNT at $TBT"
+    TBTCOUNT=$COUNT
     break
   fi
   COUNT=$((COUNT+1))
 done
 
+COUNT=0
+FOUND=0
+TCTCOUNT=0
+until [ $COUNT -gt 250 ]; do
+  TCT=$(eval echo $"elf$COUNT")
+  if [ $COUNT -eq $TBTCOUNT ]; then
+    COUNT=$((COUNT+1))
+    TCT=$(eval echo $"elf$COUNT")
+  fi
+  zCOUNT=0
+  until [ $zCOUNT -gt 250 ]; do
+    DCD=$(eval echo $"elf$zCOUNT")
+    if [ $zCOUNT -eq $TBTCOUNT ]; then
+      zCOUNT=$((zCOUNT+1))
+      DCD=$(eval echo $"elf$zCOUNT")
+    fi
+    if [ ! $TCT -ge $DCD ]; then
+      FOUND="0"
+      break
+    else
+      FOUND="1"
+    fi
+    zCOUNT=$((zCOUNT+1))
+  done
+  if [ $FOUND = "1" ]; then
+    echo "Runner up elf $COUNT has $TCT"
+    TCTCOUNT=$COUNT
+    break
+  fi
+  COUNT=$((COUNT+1))
+done
+
+COUNT=0
+FOUND=0
+until [ $COUNT -gt 250 ]; do
+  TDT=$(eval echo $"elf$COUNT")
+  if [ $COUNT -eq $TBTCOUNT ] || [ $COUNT -eq $TCTCOUNT ]; then
+    COUNT=$((COUNT+1))
+    TDT=$(eval echo $"elf$COUNT")
+  fi
+  zCOUNT=0
+  until [ $zCOUNT -gt 250 ]; do
+    DDD=$(eval echo $"elf$zCOUNT")
+    if [ $zCOUNT -eq $TBTCOUNT ] || [ $zCOUNT -eq $TCTCOUNT ]; then
+      zCOUNT=$((zCOUNT+1))
+      DDD=$(eval echo $"elf$zCOUNT")
+    fi
+    if [ ! $TDT -ge $DDD ]; then
+      FOUND="0"
+      break
+    else
+      FOUND="1"
+    fi
+    zCOUNT=$((zCOUNT+1))
+  done
+  if [ $FOUND = "1" ]; then
+    echo "Runner up elf $COUNT has $TDT"
+    break
+  fi
+  COUNT=$((COUNT+1))
+done
+
+echo "Total supplies of runner-ups is $((TCT+TDT))"
