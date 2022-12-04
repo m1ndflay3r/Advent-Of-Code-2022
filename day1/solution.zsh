c83d005706d5c597e0e68512df37338959a2c7a8
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2022-12-04 10:02:04 -0700

    simplify

--- day1/solution.zsh
+++ day1/solution.zsh
@@ -3,106 +3,24 @@
 source /$(pwd)/libraw
 
 COUNT=0
-unset SUMS
-unset RAWSUMS
-ISLASTRUN=0
-if [ -z $TERMINATEALREADY ]; then
-  TERMINATEALREADY="0"
-fi
-declare -a SUMS
-until [ $COUNT = 251 ]; do
-  if [ ! -z "$BLACKLIST" ]; then
-    if [ $COUNT = $BLACKLIST ]; then
-      COUNT=$((COUNT+1))
-    fi
-    if [ -z "$MEMBERME" ]; then
-      ISLASTRUN=0
-      export MEMBERME=$BLACKLIST
-    else
-      ISLASTRUN=1
-    fi
-    if [ $COUNT = $MEMBERME ]; then
-      COUNT=$((COUNT+1))
-    fi
-  fi
-  RET=0
-  export SOMEVAR=elf$COUNT
-  for i in $(typeset $SOMEVAR); do
-    if ! echo $i | grep \( &> /dev/null; then
-      if ! echo $i | grep \) &> /dev/null; then
-        RET=$((RET+i))
-      fi
-    fi
-  done
-  RAWSUMS+=(
-    "$RET"
-  )
-  SUMS+=(
-    "$COUNT   $RET"
-  )
-  COUNT=$((COUNT+1))
-done
-
-FOUND="0"
-unset CANDIDATES
-declare -a CANDIDATES
-for j in $RAWSUMS[@]; do
-  for k in $RAWSUMS[@]; do
-    if [ $k -gt $j ]; then
+FOUND=0
+until [ $COUNT -gt 250 ]; do
+  TBT=$(eval echo $"elf$COUNT")
+  zCOUNT=0
+  until [ $zCOUNT -gt 250 ]; do
+    DBD=$(eval echo $"elf$zCOUNT")
+    if [ ! $TBT -ge $DBD ]; then
       FOUND="0"
       break
     else
       FOUND="1"
     fi
+    zCOUNT=$((zCOUNT+1))
   done
-  if [ $FOUND = 1 ]; then
-    CANDIDATES+=(
-      "$j"
-    )
-  fi
-done
-
-for wasd in $SUMS[@]; do
-  if echo $wasd | grep $CANDIDATES[@] &> /dev/null; then
-    IFS=$'   '
-    FLAGGYMCFLAGFACE=0
-    for finale in $=wasd; do
-      if [ $FLAGGYMCFLAGFACE = 0 ]; then
-        if [ -z "$BLACKLIST" ]; then
-          echo "Elf $finale is the biggest resource hoarder of them all."
-        else
-          echo "Elf $finale is still kind of hoarding, but isn't as bad as the first guy."
-        fi
-        FLAGGYMCFLAGFACE=1
-        BLACKLIST=$finale
-      else
-        echo "Total resource count: $finale"
-        if [ ! -z "$BLACKLIST" ]; then
-          if [ -z "$FINALONE" ]; then
-            export FINALONE=$finale
-          elif [ -z "$FINALTWO" ]; then
-            export FINALTWO=$finale
-          fi
-        fi
-        echo " "
-      fi
-    done
-    if [ $ISLASTRUN = 0 ]; then
-      BLACKLIST=$BLACKLIST . /$(pwd)/solution.zsh
-    else
-      if [ $TERMINATEALREADY = 1 ]; then
-        unset MEMBERME
-        FINALFINAL=$((FINALONE+FINALTWO))
-        echo " "
-        echo "Total resource count between the two runner-up goons: $FINALFINAL"
-        unset FINALONE
-        unset FINALTWO
-        exit 0
-      else
-        TERMINATEALREADY=1 BLACKLIST=$BLACKLIST MEMBERME=$MEMBERME . /$(pwd)/solution.zsh
-      fi
-    fi
-    unset IFS
+  if [ $FOUND = "1" ]; then
+    echo $TBT
     break
   fi
+  COUNT=$((COUNT+1))
 done
+
