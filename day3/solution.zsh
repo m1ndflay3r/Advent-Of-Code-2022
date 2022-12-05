42005b3da139c18bcb60a468bb85cecd8a56d69c
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2022-12-05 15:50:47 -0700

    getting there

--- day3/solution.zsh
+++ day3/solution.zsh
@@ -1,2 +1,52 @@
 #!/usr/bin/env zsh
 
+source /$(pwd)/libpriorities
+
+INPUT=$(cat /$(pwd)/OGinput)
+TOTAL=0
+
+IFS=$'\n'
+for ii in $=INPUT; do
+  unset IFS
+  STRLN=0
+  while read -n -k 1 -u 0 character; do
+    STRLN=$((STRLN+1))
+  done < <(echo -n $ii)
+  STRLN=$((STRLN/2))
+  COMPTWO=$(echo ${ii:$STRLN})
+  COMPONE=$(echo ${ii:0:$STRLN})
+  unset COMPONEARR
+  declare -a COMPONEARR
+  while read -n -k 1 -u 0 character; do
+    COMPONEARR+=(
+      "$character"
+    )
+  done < <(echo -n $COMPONE)
+  unset COMPTWOARR
+  declare -a COMPTWOARR
+  while read -n -k 1 -u 0 character; do
+    COMPTWOARR+=(
+      "$character"
+    )
+  done < <(echo -n $COMPTWO)
+  for jj in $COMPONEARR[@]; do
+    for kk in $COMPTWOARR[@]; do
+      if [ $jj = $kk ]; then
+        unset NUMVAL
+        NUMVAL=$(eval echo $"$kk")
+        TOTAL=$((TOTAL+NUMVAL))
+        break
+        MATCHED=1
+      else
+        MATCHED=0
+      fi
+    done
+    if [ $MATCHED = 1 ]; then
+      break
+    fi
+  done
+  IFS=$'\n'
+done
+unset IFS
+echo "Total sum of priorities is $TOTAL"
+
