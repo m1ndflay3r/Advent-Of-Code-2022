3081b16f2abab96b9d5b3a3e1b84d3f12812b481
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2022-12-06 04:51:25 -0700

    day 4 continues

--- day4/solution.zsh
+++ day4/solution.zsh
@@ -2,4 +2,37 @@
 
 INPUT=$(cat ./OGinput)
 
-
+IFS=$'\n\n'
+for a in $=INPUT; do
+  IFS=$','
+  for b in $=a; do
+    IFS=$'-'
+    unset AA
+    unset BB
+    FLAGGY=0
+    for c in $=b; do
+      if [ $FLAGGY = 0 ]; then
+        AA=$c
+        FLAGGY=1
+      else
+        BB=$c
+        FLAGGY=0
+      fi
+    done
+    if [ $AA -gt $BB ]; then
+      until [ $AA = $BB ]; do
+        echo $BB
+        BB=$((BB+1))
+      done
+      echo $BB
+    elif [ $BB -gt $AA ]; then
+      until [ $BB = $AA ]; do
+        echo $AA
+        AA=$((AA+1))
+      done
+      echo $AA
+    fi
+    IFS=$','
+  done
+  IFS=$'\n\n'
+done
