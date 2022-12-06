f3366c403c0c0b0c04d44f0dc4bf15c3d4640cf4
m1ndflay3r <sp0t1fyfr33tr1alabus3@outlook.com>
2022-12-06 05:55:49 -0700

    day 4 continues

--- day4/solution.zsh
+++ day4/solution.zsh
@@ -26,18 +26,18 @@ for a in $=INPUT; do
     unset BBSTR
     if [ $AA -gt $BB ]; then
       until [ $AA = $BB ]; do
-        BBSTR=""$BBSTR" "$BB""
+        BBSTR=""$BBSTR""$BB""
         BB=$((BB+1))
       done
-      BBSTR=""$BBSTR" "$BB""
+      BBSTR=""$BBSTR""$BB""
     elif [ $BB -gt $AA ]; then
       until [ $BB = $AA ]; do
-        AASTR=""$AASTR" "$AA""
+        AASTR=""$AASTR""$AA""
         AA=$((AA+1))
       done
-      AASTR=""$AASTR" "$AA""
+      AASTR=""$AASTR""$AA""
     elif [ $AA = $BB ]; then
-      AASTR=""$AASTR" "$AA""
+      AASTR=""$AASTR""$AA""
       AA=$((AA+1))
     fi
     RETURN+=(
@@ -55,8 +55,34 @@ for i in $RETURN[@]; do
     zRETURN="$i"
   else
     zFLAG=0
-    zRETURN=""$zRETURN" :"$i""
+    zRETURN=""$zRETURN":"$i""
     echo $zRETURN >> /$(pwd)/expandedinput
-    echo "" >> /$(pwd)/expandedinput
   fi
 done
+
+EXPINPUT=$(cat ./expandedinput)
+IFS=$'\n'
+zFLAG=0
+unset COMPAREONE
+unset COMPARETWO
+FINALCOUNT=0
+for i in $=EXPINPUT; do
+  IFS=$':'
+  unset COMPAREONE
+  unset COMPARETWO
+  for j in $=i; do
+    if [ $zFLAG = 0 ]; then
+      COMPAREONE="$j"
+      zFLAG=1
+    else
+      COMPARETWO="$j"
+      zFLAG=0
+    fi
+  done
+  if echo -n $COMPAREONE | grep $COMPARETWO &> /dev/null; then
+    FINALCOUNT=$((FINALCOUNT+1))
+  elif echo -n $COMPARETWO | grep $COMPAREONE &> /dev/null; then
+    FINALCOUNT=$((FINALCOUNT+1))
+  fi
+done
+echo $FINALCOUNT
